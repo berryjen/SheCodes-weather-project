22f5cbf51e8d0da02621501ab791e4b7ff26002e
Jen <73888356+berryjen@users.noreply.github.com>
2021-01-23 12:39:47 +0000

    added celsiusTemperature= response.data.main.temp;

--- index.js
+++ index.js
@@ -75,6 +75,8 @@ function displayWeatherCondition(response) {
   icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
+  celsiusTemperature= response.data.main.temp;
+ 
 }
 
 function searchCity(city) {
