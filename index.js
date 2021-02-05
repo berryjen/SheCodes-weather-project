e49e9698af5e295a8188b4b10f6f9277578cf7e5
Jen <73888356+berryjen@users.noreply.github.com>
2021-02-05 20:50:25 +0000

    added axios call to function searchLocation

--- index.js
+++ index.js
@@ -137,6 +137,8 @@ function searchLocation(position) {
   let apiKey = "f080158c041532d07353f9c3c3fc3150";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(displayForecast);
+}
 }
 let unit = "metric";
 
